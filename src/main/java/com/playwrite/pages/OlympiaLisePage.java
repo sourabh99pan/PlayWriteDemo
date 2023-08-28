dc4a30e12f507b168c991a300642defecf03ac03
sourabh99pan <sourabhpan11@gmail.com>
2023-08-28 15:56:23 +0530

    added on 28th Aug

--- src/main/java/com/playwrite/pages/OlympiaLisePage.java
+++ src/main/java/com/playwrite/pages/OlympiaLisePage.java
@@ -2,58 +2,94 @@ package com.playwrite.pages;
 
 import java.util.*;
 
+import org.testng.Assert;
+
 import com.microsoft.playwright.Page;
 
 public class OlympiaLisePage {
-	
+
 	Page page;
-	
+
 	public String listOption = "//span[contains(@class,'layout-menuitem-text ng-tns-') and text()='List' ]";
-	
+
 	public String countrySelect = "(//div[@class='ng-star-inserted'][normalize-space()='*'])[1]";
-	
+
 	public String selectButton = "//div[@class='p-picklist-buttons p-picklist-transfer-buttons']//button[1]";
-	
+
 	public String insertedCountry = "(//div[@class='ng-star-inserted'][normalize-space()='*'])[1]";
+
+	public String paginationNumber = "//button[normalize-space()='*']";
+
+	public String searchBox = "//input[@placeholder='Search by Name']";
 	
-	//Page constructor
+	public String productOnList = "//div[normalize-space()='*']";
 	
-	public OlympiaLisePage(Page page)
-	{
-		this.page=page;
+	public String priceOnList = "//span[normalize-space()='*']";
+
+	// Page constructor
+
+	public OlympiaLisePage(Page page) {
+		this.page = page;
 	}
-	
-	public void dragAndDrop(String country)
-	{
+
+	public void dragAndDrop(String country) {
 
 		page.click(countrySelect.replace("*", country));
 		page.click(selectButton);
-		
+
 	}
-	
-	public void clickOption()
-	{
+
+	public void clickOption() {
 		page.click(listOption);
 	}
-	
-	public void verifyCountry(String country)
-	{
+
+	public void verifyCountry(String country) {
 		page.isVisible(insertedCountry.replace("*", country));
 	}
-	
-	public void dragAndDrop(List <String>country)
-	{
-		for(String c:country)
-		{
+
+	public void dragAndDrop(List<String> country) {
+		for (String c : country) {
 			page.click(countrySelect.replace("*", c));
 			page.click(selectButton);
-		}		
+		}
+	}
+
+	public void verifyCountry(List<String> country) {
+		for (String c : country) {
+			page.isVisible(insertedCountry.replace("*", c));
+		}
+	}
+
+	public void clickPagination() {
+
+		int count = 1;
+		page.waitForSelector(paginationNumber.replace("*", Integer.toString(count).replace("['\']", "")));
+		System.out.println(page.isVisible(paginationNumber.replace("*", Integer.toString(count).replace("['\']", ""))));
+
+		while (page.isVisible(paginationNumber.replace("*", Integer.toString(count)))) {
+			// page.pause();
+			page.click(paginationNumber.replace("*", Integer.toString(count)));
+			count++;
+		}
+
 	}
 	
-	public void verifyCountry(List <String>country)
-	{	
-		for(String c:country) {
-			page.isVisible(insertedCountry.replace("*", c));
+	public void searchProduct(String productName, String price)
+	{
+		page.fill(searchBox, productName);
+		Assert.assertEquals(page.isVisible(productOnList.replace("*", productName)), true);
+		Assert.assertEquals(page.isVisible(priceOnList.replace("*", price)), true);
+
+	}
+	
+	public void searchProduct(HashMap<String, String> productDet)
+	{
+		for(Map.Entry<String, String> en:productDet.entrySet())
+		{
+			page.fill(searchBox, en.getKey());
+			Assert.assertEquals(page.isVisible(productOnList.replace("*", en.getKey())), true,en.getKey()+"Product name not match");
+			Assert.assertEquals(page.isVisible(priceOnList.replace("*", en.getValue())), true,en.getKey()+" "+en.getValue()+"Price does not match");
+			page.fill(searchBox, "");
 		}
 	}
 
--- src/test/java/com/playwright/olympia/ListSelection.java
+++ src/test/java/com/playwright/olympia/ListSelection.java
@@ -1,7 +1,6 @@
 package com.playwright.olympia;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 import org.testng.annotations.Test;
 
@@ -10,37 +9,67 @@ import com.playwrite.base.TestBase;
 import com.playwrite.pages.OlympiaLisePage;
 import com.playwrite.pages.OlympiaTablePage;
 
-public class ListSelection extends TestBase{
+public class ListSelection extends TestBase {
 
 	OlympiaLisePage olympiaListpage;
-	
-	@Test
-	public void TC011() throws InterruptedException
-	{
-		
+
+	@Test(enabled = false)
+	public void TC011() throws InterruptedException {
+
 		olympiaListpage = new OlympiaLisePage(page);
 		page.navigate(prop.getProperty("olympiaUrl"));
-		
+
 		olympiaListpage.clickOption();
 		olympiaListpage.dragAndDrop("Istanbul");
 		olympiaListpage.verifyCountry("Istanbul");
 
-		
-		
+	}
+
+	@Test(enabled = false)
+	public void TC012() throws InterruptedException {
+
+		olympiaListpage = new OlympiaLisePage(page);
+		page.navigate(prop.getProperty("olympiaUrl"));
+
+		olympiaListpage.clickOption();
+		ArrayList<String> countryList = new ArrayList<>(Arrays.asList("Istanbul", "Paris", "Berlin", "Rome"));
+		olympiaListpage.dragAndDrop(countryList);
+
+		olympiaListpage.verifyCountry(countryList);
+
+	}
+
+	@Test(enabled = false)
+	public void TC013() throws InterruptedException {
+
+		olympiaListpage = new OlympiaLisePage(page);
+		page.navigate(prop.getProperty("olympiaUrl"));
+
+		olympiaListpage.clickOption();
+
+		olympiaListpage.clickPagination();
+
 	}
 	
 	@Test
-	public void TC012() throws InterruptedException
-	{
-		
+	public void TC014() throws InterruptedException {
+
 		olympiaListpage = new OlympiaLisePage(page);
 		page.navigate(prop.getProperty("olympiaUrl"));
-		
+
 		olympiaListpage.clickOption();
-		ArrayList<String> countryList = new ArrayList<>(Arrays.asList("Istanbul","Paris","Berlin","Rome"));
-		olympiaListpage.dragAndDrop(countryList);
 		
-		olympiaListpage.verifyCountry(countryList);
-	
+		HashMap<String, String> map = new <String, String>LinkedHashMap();
+		//olympiaListpage.searchProduct("Bamboo Watch", "$65");
+		map.put("Bamboo Watch", "$65");
+		map.put("Black Watch", "$72");
+		map.put("Blue Band", "$79");
+		map.put("Bracelet", "$15");
+		map.put("Shoes", "$64");
+		map.put("Brown Purse", "$120");
+		map.put("Sneakers", "$78");
+		map.put("Purple Band", "$79");
+		olympiaListpage.searchProduct(map);
+
 	}
 }

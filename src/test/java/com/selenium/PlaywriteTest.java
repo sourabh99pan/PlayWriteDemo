5514e1cb8b2e53d9716b2a6f03ac4d79361750aa
sourabh99pan <sourabhpan11@gmail.com>
2025-04-20 13:22:42 +0530

    add gitdesktop

--- src/test/java/com/selenium/PlaywriteTest.java
+++ src/test/java/com/selenium/PlaywriteTest.java
@@ -15,7 +15,7 @@ public class PlaywriteTest extends TestBase {
 	public void TC_001()
 	{
 			      System.out.println("in Test TC_001");
-			      System.out.println("update from local branch");
+			      System.out.println("check git desktop PR");
             page.navigate("http://google.com");
             System.out.println(page.title());
             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("TC_001.png")));
